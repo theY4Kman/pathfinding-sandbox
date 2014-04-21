fe3ed99883e090ad333d88fcfdedd98b461be5e8
Zach Kanzler <they4kman@gmail.com>
2014-04-20 20:03:22 -0400

    Procedural generation of walls and start/end points

--- pathfinding.py
+++ pathfinding.py
@@ -1,5 +1,11 @@
-from queue import PriorityQueueSet
 import math
+import os
+import random
+
+from queue import PriorityQueueSet
+
+import pygame
+pygame.init()
 
 
 class Board(object):
@@ -37,13 +43,22 @@ class Board(object):
         return (self.width * self.cell_size + self.grid_line_width,
                 self.height * self.cell_size + self.grid_line_width)
 
-    def is_valid_point(self, x, y):
+    def is_in_board(self, (x, y)):
         return (0 <= x <= self.width and 0 <= y <= self.height)
 
-    def is_valid_path_point(self, x, y):
-        return self.is_valid_point(x, y) and (x, y) not in self.walls
+    def is_valid_path_point(self, (x, y)):
+        return self.is_in_board((x, y)) and not self.is_wall((x, y))
 
-    def dist_to_end(self, x, y):
+    def is_wall(self, point):
+        return point in self.walls
+
+    def get_open_points(self):
+        all_points = [(x, y)
+                      for x in xrange(self.width)
+                      for y in xrange(self.height)]
+        return filter(lambda p: not self.is_wall(p), all_points)
+
+    def dist_to_end(self, (x, y)):
         end_x, end_y = self.end_point
         return math.sqrt((end_x - x) ** 2 + (end_y - y) ** 2)
 
@@ -65,7 +80,7 @@ class Board(object):
             return
         else:
             x, y = self.start_point
-            self._draw_circle(surface, x, y,
+            self._draw_circle(surface, (x, y),
                               self.start_point_radius, self.start_point_color)
 
     def draw_end_point(self, surface):
@@ -73,7 +88,7 @@ class Board(object):
             return
         else:
             x, y = self.end_point
-            self._draw_circle(surface, x, y,
+            self._draw_circle(surface, (x, y),
                               self.end_point_radius, self.end_point_color)
 
     def draw_path(self, surface):
@@ -84,7 +99,7 @@ class Board(object):
 
         last_x, last_y = self.start_point
         for x, y in self.path:
-            if not self.is_valid_point(x, y):
+            if not self.is_in_board((x, y)):
                 raise ValueError('Invalid path coordinate ({}, {})'.format(x,
                                                                            y))
             source = (last_x * self.cell_size, last_y * self.cell_size)
@@ -96,7 +111,7 @@ class Board(object):
 
     def draw_walls(self, surface):
         for c_x, c_y in self.walls:
-            if not self.is_valid_point(c_x, c_y):
+            if not self.is_in_board((c_x, c_y)):
                 raise ValueError('Invalid location ({}, {}) '
                                  'for wall'.format(c_x, c_y))
 
@@ -112,7 +127,7 @@ class Board(object):
         self.draw_start_point(surface)
         self.draw_end_point(surface)
 
-    def _draw_circle(self, surface, x, y, r, color):
+    def _draw_circle(self, surface, (x, y), r, color):
         pygame.draw.circle(surface, color, (x * self.cell_size,
                                             y * self.cell_size), r)
 
@@ -127,7 +142,7 @@ class Pathfinder(object):
         if self.board.end_point is None:
             raise ValueError('Board has no end point set')
 
-    def get_valid_moves(self, x, y):
+    def get_valid_moves(self, (x, y)):
         moves = []
         if x > 0:
             moves.append((x - 1, y))
@@ -140,8 +155,8 @@ class Pathfinder(object):
         return moves
 
     def get_available_moves(self, x, y):
-        return filter(lambda p: self.board.is_valid_path_point(*p),
-                      self.get_valid_moves(x, y))
+        return filter(self.board.is_valid_path_point,
+                      self.get_valid_moves((x, y)))
 
     def __iter__(self):
         return self
@@ -161,15 +176,14 @@ class SimplePathfinder(Pathfinder):
         if (self.x, self.y) == self.board.end_point:
             raise StopIteration
 
-        valid_moves = self.get_valid_moves(self.x, self.y)
+        valid_moves = self.get_valid_moves((self.x, self.y))
         new_moves = filter(lambda p: p not in self.already_taken, valid_moves)
-        available_moves = filter(lambda p: self.board.is_valid_path_point(*p),
-                                 new_moves)
+        available_moves = filter(self.board.is_valid_path_point, new_moves)
 
         least_dist = float('Inf')
         least_point = None
         for point in available_moves:
-            dist = self.board.dist_to_end(*point)
+            dist = self.board.dist_to_end(point)
             if dist < least_dist:
                 least_dist = dist
                 least_point = point
@@ -223,85 +237,166 @@ class BestFirstSearchPathfinder(Pathfinder):
         return self.find_path()
 
 
-def get_board():
-    board = Board()
+class SimpleWallGenerator(object):
+    change_direction_chance = 0.3
 
-    layout = [
-        '   s #   #   #   #   #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '     # # # # # # # # #       ',
-        '       # # # # # # # # e     ',
-        '       # # # # # # # #       ',
-        '       # # # # # # # #       ',
-        '       # # # # # # # #       ',
-        '       #   #   #   #         ',
-    ]
+    def __init__(self, board):
+        self.board = board
 
-    for y, row in enumerate(layout):
-        for x, c in enumerate(row):
-            if c == '#':
-                board.walls.append((x, y))
-            elif c.lower() == 's':
-                board.start_point = (x, y)
-            elif c.lower() == 'e':
-                board.end_point = (x, y)
+        self.open_points = board.get_open_points()
 
-    return board
+    @staticmethod
+    def get_random_direction_vector():
+        return random.choice([
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1),
+        ])
+
+    def get_positions_ahead(self, (x, y), direction):
+        """Gets the points straight forward and diagonal to the passed point
+
+        -------
+        |a|b|c|
+        -------  For point 'e', (1,1), and direction right, (1, 0), this will
+        |d|e|f|  return ['c', 'f', 'i'], which is [(2, 1), (2, 0), (2, 2)]
+        -------
+        |g|h|i|
+        -------
+        """
+        if all(direction):
+            raise ValueError('direction must be a normalized vector (x, y) '
+                             'with either x or y as 1')
+
+        dir_x, dir_y = direction
+        if dir_x:
+            a_d_x = [dir_x] * 3
+        else:
+            a_d_x = [-1, 0, 1]
+
+        if dir_y:
+            a_d_y = [dir_y] * 3
+        else:
+            a_d_y = [-1, 0, 1]
+
+        positions = [(x + d_x, y + d_y) for d_x, d_y in zip(a_d_x, a_d_y)]
+        return filter(self.board.is_in_board, positions)
+
+    def get_turn_vectors(self, (dir_x, dir_y)):
+        if dir_x and dir_y:
+            raise ValueError('direction must be a normalized vector (x, y) '
+                             'with either x or y as 1')
+        if dir_x:
+            return [(0, 1), (0, -1)]
+        if dir_y:
+            return [(1, 0), (-1, 0)]
+
+    def generate_wall(self, point, length):
+        direction = self.get_random_direction_vector()
+        for _ in xrange(length):
+            # Detects if the wall is outside the board. The conflict
+            # detection should never allow a wall to be inside another wall
+            if point not in self.open_points:
+                return
+
+            possible_conflicts = self.get_positions_ahead(point, direction)
+            for pos in possible_conflicts:
+                if pos not in self.open_points:
+                    return
+
+            self.board.walls.append(point)
+            self.open_points.remove(point)
+
+            # Change direction
+            if random.random() < self.change_direction_chance:
+                direction = random.choice(self.get_turn_vectors(direction))
+
+            x, y = point
+            d_x, d_y = direction
+            point = (x + d_x, y + d_y)
+
+    def build_walls(self):
+        # Don't know what to call this arbitrary number related in some way to the
+        # size of the board.
+        board_alpha = int(math.ceil(math.log(self.board.width
+                                             * self.board.height)))
+        num_walls = random.randint(board_alpha, board_alpha * 2)
+
+        for _ in xrange(num_walls):
+            length = random.randint(board_alpha * 2, board_alpha ** 2)
+            point = random.choice(self.open_points)
+            self.generate_wall(point, length)
 
 
-def main():
-    pass
+class Sandbox(object):
+    padding = 10
+
+    def __init__(self):
+        self.reset()
+        self.initialize_screen()
+
+    def reset(self):
+        self.board = Board(start=(0, 0), end=(30, 24))
+
+        self.wall_gen = SimpleWallGenerator(self.board)
+        self.wall_gen.build_walls()
+
+        self.set_start_end_points()
+
+        self.pathfinder = BestFirstSearchPathfinder(self.board)
+        self.path_iter = iter(self.pathfinder.find_path())
+
+    def set_start_end_points(self):
+        open_points = self.board.get_open_points()
+        self.board.start_point = random.choice(open_points)
+
+        open_points.remove(self.board.start_point)
+        self.board.end_point = random.choice(open_points)
+
+    def initialize_screen(self):
+        width, height = self.board.size()
+        self.screen = pygame.display.set_mode((width + self.padding * 2,
+                                               height + self.padding * 2))
+        self.surface = pygame.Surface((width, height))
+
+    def mainloop(self):
+        STATE_DRAWING = 0
+        STATE_SHOWING = 1
+
+        RESETEVENT = pygame.USEREVENT + 1
+
+        state = STATE_DRAWING
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    os._exit(0)
+                elif event.type == RESETEVENT:
+                    self.reset()
+                    state = STATE_DRAWING
+                    # Cancel repeating timer
+                    pygame.time.set_timer(RESETEVENT, 0)
+
+            self.surface.fill((255, 255, 255))
+            self.board.render(self.surface)
+
+            self.screen.fill((255, 255, 255))
+            self.screen.blit(self.surface, (self.padding, self.padding))
+
+            pygame.display.flip()
+
+            if state == STATE_DRAWING:
+                try:
+                    next_point = self.path_iter.next()
+                except StopIteration:
+                    pygame.time.set_timer(RESETEVENT, 1000)
+                    state = STATE_SHOWING
+                else:
+                    pygame.time.wait(100)
+                    self.board.path.append(next_point)
 
 
 if __name__ == '__main__':
-    import os
-    import pygame
-    pygame.init()
-
-    board = get_board()
-    pathfinder = BestFirstSearchPathfinder(board)
-    path = iter(pathfinder.find_path())
-
-    width, height = board.size()
-    padding = 10
-    screen = pygame.display.set_mode((width + padding * 2,
-                                      height + padding * 2))
-    surface = pygame.Surface((width, height))
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                os._exit(0)
-
-        surface.fill((255, 255, 255))
-        board.render(surface)
-
-        screen.fill((255, 255, 255))
-        screen.blit(surface, (padding, padding))
-
-        pygame.display.flip()
-
-        try:
-            next_point = path.next()
-        except StopIteration:
-            pass
-        else:
-            pygame.time.wait(100)
-            board.path.append(next_point)
+    sandbox = Sandbox()
+    sandbox.mainloop()
